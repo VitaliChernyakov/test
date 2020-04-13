2109652405573c03fbe7cf5c714ed09e3a0ad5eb
Vitali <vitali.chernyakov@gmail.com>
2020-04-13 15:58:36 +0300

    Second

--- src/main/java/config/IndexController.java
+++ src/main/java/config/IndexController.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+    public String name;
+
     @GetMapping ({"/app"})
     public  String handle(Model model){
         model.addAttribute("message", "Hello world!");
         return "index";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class HomeController {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
